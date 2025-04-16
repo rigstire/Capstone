91f2feba2e7b25d19296d6eeb901b87e84ebe824
rigstire <brodsky.nick@gmail.com>
2025-04-16 09:44:51 -0700

    msg

--- capstone/aimodels/admin.py
+++ capstone/aimodels/admin.py
@@ -3,16 +3,16 @@ from .models import AIModel
 
 @admin.register(AIModel)
 class AIModelAdmin(admin.ModelAdmin):
-    list_display = ("name", "runtime", "architecture", "parameters", "release_date", "created_at")
-    search_fields = ("name", "runtime", "architecture")
-    list_filter = ("architecture", "release_date")
+    list_display = ("name", "parameters", "created_at")
+    search_fields = ("name",)
+    list_filter = ( "accuracy",)
     readonly_fields = ("created_at",)
     fieldsets = (
         ("Model Information", {
-            "fields": ("name", "runtime", "architecture", "parameters", "description")
+            "fields": ("name", "parameters", "description", "accuracy")
         }),
         ("Metadata", {
-            "fields": ("release_date", "created_at")
+            "fields": ( "created_at",)
         }),
     )
 
--- capstone/benchmark/models.py
+++ capstone/benchmark/models.py
@@ -6,8 +6,8 @@ class Benchmark(models.Model):
     ai_model = models.ForeignKey('aimodels.AIModel', on_delete=models.CASCADE, related_name="benchmarks")
     device = models.ForeignKey('device.Device', on_delete=models.CASCADE, related_name="benchmarks")
     test_date = models.DateField(auto_now_add=True)
+    runtime = models.CharField(blank=True,null=True, max_length=50000)
     inference_time = models.FloatField(help_text="Time in milliseconds")  # Lower is better
-    accuracy = models.FloatField(help_text="Accuracy as a percentage")  # E.g., 95.3 for 95.3%
     memory_usage = models.FloatField(blank=True, null=True, help_text="Power used in watts")
     compute_units = models.FloatField(blank=True, null= True, help_text="Number of compute units used for image processing")
     notes = models.TextField(blank=True, null=True)
